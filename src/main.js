39f0d038f0b5df21f25100cddce0c393c6cc3b71
betogroo <luizhumberto@gmail.com>
2021-10-05 17:23:36 -0300

    feat: configure global components with prefix "App"

--- src/main.js
+++ src/main.js
@@ -1,15 +1,37 @@
 import { createApp } from 'vue'
-import vuetify from './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify'
 import { fbAuth } from '@/plugins/firebase.js'
+
+import upperFirst from 'lodash/upperFirst'
+import camelCase from 'lodash/camelCase'
+
+const requireComponent = require.context(
+  './components/app',
+  false,
+  /App[A-Z]\w+\.(vue|js)$/
+)
 
 let app
 
 fbAuth.onAuthStateChanged(() => {
   if (!app) {
     app = createApp(App)
+    requireComponent.keys().forEach((fileName) => {
+      const componentConfig = requireComponent(fileName)
+      const componentName = upperFirst(
+        camelCase(
+          fileName
+            .split('/')
+            .pop()
+            .replace(/\.\w+$/, '')
+        )
+      )
+
+      app.component(componentName, componentConfig.default || componentConfig)
+    })
     app.use(router)
     app.use(store)
     app.use(vuetify).mount('#app')
