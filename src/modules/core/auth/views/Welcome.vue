ffe18f67229aa2dd171fc12bc4c17381f1f132eb
betogroo <luizhumberto@gmail.com>
2021-10-06 12:58:05 -0300

    refactor: port welcome view to v3 (in progress)

--- src/modules/core/auth/views/Welcome.vue
+++ src/modules/core/auth/views/Welcome.vue
@@ -1,35 +1,98 @@
 <template>
-  <h1>{{ title }}</h1>
-  <h3>{{ subtitle }}</h3>
-  <div v-if="showForm === 'login'" class="d-flex flex-column">
-    <login-form @login="gotoHome"></login-form>
-    <a @click.prevent="handleForm('signup')"
-      >Não tem conta? <strong>Cadastre-se</strong></a
+  <app-card flat :class="$vuetify.display.mdAndDown ? 'mobile' : 'desktop'">
+    <app-card
+      flat
+      :class="
+        $vuetify.display.mdAndDown
+          ? 'mobile-banner-card'
+          : 'desktop-banner-card'
+      "
+      color="default"
     >
-    <a @click.prevent="handleForm('reset')"
-      >Esqueceu a senha? <strong>Redefina</strong></a
+    </app-card>
+
+    <app-card
+      flat
+      color="transparent"
+      :class="$vuetify.display.mdAndDown ? 'mobile-card' : 'desktop-card'"
     >
-  </div>
-  <div v-if="showForm === 'signup'" class="d-flex flex-column">
-    <signup-form @signup="gotoHome"></signup-form>
-    <a @click.prevent="handleForm('login')"
-      >Já tem conta? <strong>Faça Login</strong></a
-    >
-  </div>
-  <div v-if="showForm === 'reset'" class="d-flex flex-column">
-    <reset-password-form></reset-password-form>
-    <a @click.prevent="handleForm('login')"
-      >Já tem conta? <strong>Faça Login</strong></a
-    >
-  </div>
+      <div
+        class="d-flex flex-column text-center pb-6"
+        :class="$vuetify.display.mdAndDown ? 'white--text' : 'default--text'"
+      >
+        <span class="text-h4 pb-2">{{ title }}</span>
+        <span class="text-subtitle">{{ subtitle }}</span>
+      </div>
+      <app-card
+        flat
+        :width="cardWidth"
+        :class="
+          $vuetify.display.mdAndDown
+            ? 'mobile-login-card'
+            : 'desktop-login-card'
+        "
+      >
+        <div v-if="showForm === 'login'">
+          <login-form @login="gotoHome"></login-form>
+          <v-row no-gutters>
+            <v-col cols="12" class="text-center pt-4">
+              <a
+                class="text-decoration-none default--text"
+                @click.prevent="handleForm('signup')"
+              >
+                Ainda não tem conta?
+                <strong> Cadastre-se {{ name }} </strong>
+              </a>
+            </v-col>
+          </v-row>
+          <v-row no-gutters>
+            <v-col cols="12" class="text-center pt-4">
+              <a
+                class="text-decoration-none default--text"
+                @click.prevent="handleForm('reset')"
+              >
+                Esqueceu a senha?
+                <strong> Redefina</strong>
+              </a>
+            </v-col>
+          </v-row>
+        </div>
+        <div v-if="showForm === 'signup'">
+          <signup-form @signup="gotoHome"></signup-form>
+          <v-row no-gutters>
+            <v-col cols="12" class="text-center pt-4">
+              <a
+                class="text-decoration-none default--text"
+                @click.prevent="handleForm('login')"
+              >
+                Já é cadastrado? Faça o <strong> login </strong>
+              </a>
+            </v-col>
+          </v-row>
+        </div>
+        <div v-if="showForm === 'reset'">
+          <reset-password-form @reset="gotoHome"></reset-password-form>
+          <v-row no-gutters>
+            <v-col cols="12" class="text-center pt-4">
+              <a
+                class="text-decoration-none default--text"
+                @click.prevent="handleForm('login')"
+              >
+                Já é cadastrado? Faça o <strong> login </strong>
+              </a>
+            </v-col>
+          </v-row>
+        </div>
+      </app-card>
+    </app-card>
+  </app-card>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
+import utils from '@/utils/utils.js'
 import router from '@/router'
-import SignupForm from '../components/SignupForm.vue'
-import LoginForm from '../components/LoginForm.vue'
-import ResetPasswordForm from '../components/ResetPasswordForm.vue'
+import { SignupForm, LoginForm, ResetPasswordForm } from '../components'
 export default {
   name: 'Welcome',
 
@@ -58,14 +121,73 @@ export default {
         title.value = 'Cadastro'
         subtitle.value = 'Cadastre-se para ter acesso ao sistema'
       }
-      /* if (form === 'reset') {
+      if (form === 'reset') {
         title.value = 'Redefinir Senha'
         subtitle.value = 'Entre com seu email para redefinir a senha'
-      } */
+      }
     }
-    return { showForm, title, subtitle, gotoHome, handleForm }
+
+    const cardWidth = computed(() => {
+      return utils.breakpointSize()
+    })
+    return {
+      showForm,
+      cardWidth,
+      title,
+      subtitle,
+      gotoHome,
+      handleForm,
+      name
+    }
   }
 }
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.mobile-card {
+  margin-top: -150px;
+}
+.desktop-card {
+  margin-top: auto;
+  margin-bottom: auto;
+}
+.mobile {
+  background-color: rgba(0, 0, 0, 0.01);
+  display: flex;
+  height: 100vh;
+  flex-direction: column;
+}
+.desktop {
+  height: 100vh;
+  display: flex;
+  flex-direction: row-reverse;
+  align-items: stretch;
+}
+.mobile-banner-card {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 238px;
+  border-bottom-left-radius: 8px !important;
+  border-bottom-right-radius: 8px !important;
+}
+.mobile-login-card {
+  padding-top: 20px;
+  padding-bottom: 20px;
+  margin-right: auto;
+  margin-left: auto;
+  box-shadow: 0 2px 4px -1px rgba(0, 0, 0, 0.2), 0 4px 5px 0 rgba(0, 0, 0, 0.14),
+    0 1px 10px 0 rgba(0, 0, 0, 0.12) !important;
+}
+.desktop-banner-card {
+  width: 100vw;
+  margin-right: auto;
+}
+.desktop-login-card {
+  display: flex;
+  width: 66vw;
+  align-items: center;
+  margin-right: auto;
+  margin-left: auto;
+}
+</style>
