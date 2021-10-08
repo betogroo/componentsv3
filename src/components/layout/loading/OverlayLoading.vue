3b904539b6673c6752b5c47bf66d49b7a58a98d5
betogroo <luizhumberto@gmail.com>
2021-10-08 11:12:40 -0300

    feat: create OverlayLoading

---
+++ src/components/layout/loading/OverlayLoading.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-overlay :absolute="true" :model-value="active">
+    <Bars />
+  </v-overlay>
+</template>
+
+<script>
+import { Bars } from '../spinners'
+export default {
+  name: 'OverlayLoading',
+
+  components: {
+    Bars
+  },
+
+  props: {
+    active: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
