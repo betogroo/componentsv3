8da32452b8149b56ec9a233a25768d06c2eaa29d
betogroo <luizhumberto@gmail.com>
2021-10-08 07:06:35 -0300

    feat: create global component app-icon

---
+++ src/components/app/AppIcon.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-icon v-bind="$attrs">
+    {{ `mdi-${icon}` }}
+  </v-icon>
+</template>
+
+<script>
+export default {
+  name: 'AppIcon',
+
+  props: {
+    icon: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style></style>
