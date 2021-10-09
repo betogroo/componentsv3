2e51d9b0ac538e6e9000f553e4914f28c705ab87
betogroo <luizhumberto@gmail.com>
2021-10-09 19:06:07 -0300

    refactor: select used Vuetify Components (a la carte)

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,13 +1,39 @@
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/lib/styles/main.sass'
 import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/lib/components'
-import * as directives from 'vuetify/lib/directives'
+import {
+  VApp,
+  VMain,
+  VCol,
+  VRow,
+  VCard,
+  VBtn,
+  VOverlay,
+  VIcon,
+  VAppBar,
+  VAppBarTitle,
+  VDivider,
+  VSpacer
+} from 'vuetify/lib/components'
+// import * as directives from 'vuetify/lib/directives'
 
 export default createVuetify({
   display: {
     mobileBreakpoint: 'md'
   },
-  components,
-  directives
+  components: {
+    VApp,
+    VMain,
+    VCol,
+    VRow,
+    VCard,
+    VBtn,
+    VOverlay,
+    VIcon,
+    VAppBar,
+    VAppBarTitle,
+    VDivider,
+    VSpacer
+  }
+  // directives
 })
