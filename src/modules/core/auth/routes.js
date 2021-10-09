690bcb678959e673ed9eae5a1d4f62b3afc42add
betogroo <luizhumberto@gmail.com>
2021-10-09 18:04:26 -0300

    feat: add validate param guard to welcome route

--- src/modules/core/auth/routes.js
+++ src/modules/core/auth/routes.js
@@ -1,3 +1,13 @@
+const validateParam = (to, from, next) => {
+  const validParams = ['login', 'signup', 'reset', '']
+  if (validParams.includes(to.params.mode)) {
+    next()
+  } else {
+    console.log(from)
+    next({ path: '/welcome' })
+  }
+}
+
 export default [
   {
     path: '/welcome/:mode?',
@@ -5,6 +15,7 @@ export default [
     component: () =>
       import(/* webpackChunkName: "welcome" */ './views/Welcome'),
     props: (route) => ({ mode: route.params.mode || 'login' }),
+    beforeEnter: validateParam,
     meta: {
       title: 'Welcome',
       hideNav: true,
